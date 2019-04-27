63f16ad5f46c0cfddde06043a91469385c03f3df
Carmelo <carmeloehernandezquinter@student.hartnell.edu>
2019-04-27 15:31:59 -0700

    Graphics

---
+++ Piano project/Piano project/Piano project.cpp
@@ -0,0 +1,12 @@
+#include "Piano project.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+void Main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Pianoproject::Pianoproject form;
+	Application::Run(%form);
+}
