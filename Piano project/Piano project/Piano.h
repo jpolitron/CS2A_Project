594f1184684d91f258b7182d10598a0a40c06804
Carmelo <carmeloehernandezquinter@student.hartnell.edu>
2019-05-17 11:01:58 -0700

    Graphical Piano

--- Piano project/Piano project/Piano.h
+++ Piano project/Piano project/Piano.h
@@ -1,4 +1,9 @@
 #pragma once
+//**********************************************************************
+//Team members: Carmelo Hernandez, Jessica Politron, Guadalupe Cisneros
+//Project: Graphical Piano
+//Date modified: May 17 2019
+//**********************************************************************
 
 namespace Piano {
 
@@ -104,10 +109,9 @@ namespace Piano {
 			// 
 			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button1->Location = System::Drawing::Point(45, 55);
-			this->button1->Margin = System::Windows::Forms::Padding(8, 7, 8, 7);
+			this->button1->Location = System::Drawing::Point(17, 23);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(176, 727);
+			this->button1->Size = System::Drawing::Size(66, 305);
 			this->button1->TabIndex = 0;
 			this->button1->Text = L"C";
 			this->button1->TextAlign = System::Drawing::ContentAlignment::BottomCenter;
@@ -118,10 +122,9 @@ namespace Piano {
 			// 
 			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button2->Location = System::Drawing::Point(237, 55);
-			this->button2->Margin = System::Windows::Forms::Padding(8, 7, 8, 7);
+			this->button2->Location = System::Drawing::Point(89, 23);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(176, 727);
+			this->button2->Size = System::Drawing::Size(66, 305);
 			this->button2->TabIndex = 1;
 			this->button2->Text = L"D";
 			this->button2->TextAlign = System::Drawing::ContentAlignment::BottomCenter;
@@ -132,10 +135,9 @@ namespace Piano {
 			// 
 			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button3->Location = System::Drawing::Point(429, 55);
-			this->button3->Margin = System::Windows::Forms::Padding(8, 7, 8, 7);
+			this->button3->Location = System::Drawing::Point(161, 23);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(176, 727);
+			this->button3->Size = System::Drawing::Size(66, 305);
 			this->button3->TabIndex = 2;
 			this->button3->Text = L"E";
 			this->button3->TextAlign = System::Drawing::ContentAlignment::BottomCenter;
@@ -146,10 +148,9 @@ namespace Piano {
 			// 
 			this->button4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button4->Location = System::Drawing::Point(621, 57);
-			this->button4->Margin = System::Windows::Forms::Padding(8, 7, 8, 7);
+			this->button4->Location = System::Drawing::Point(233, 24);
 			this->button4->Name = L"button4";
-			this->button4->Size = System::Drawing::Size(176, 727);
+			this->button4->Size = System::Drawing::Size(66, 305);
 			this->button4->TabIndex = 3;
 			this->button4->Text = L"F";
 			this->button4->TextAlign = System::Drawing::ContentAlignment::BottomCenter;
@@ -160,10 +161,9 @@ namespace Piano {
 			// 
 			this->button5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button5->Location = System::Drawing::Point(813, 57);
-			this->button5->Margin = System::Windows::Forms::Padding(8, 7, 8, 7);
+			this->button5->Location = System::Drawing::Point(305, 24);
 			this->button5->Name = L"button5";
-			this->button5->Size = System::Drawing::Size(176, 727);
+			this->button5->Size = System::Drawing::Size(66, 305);
 			this->button5->TabIndex = 4;
 			this->button5->Text = L"G";
 			this->button5->TextAlign = System::Drawing::ContentAlignment::BottomCenter;
@@ -174,10 +174,9 @@ namespace Piano {
 			// 
 			this->button6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button6->Location = System::Drawing::Point(1005, 57);
-			this->button6->Margin = System::Windows::Forms::Padding(8, 7, 8, 7);
+			this->button6->Location = System::Drawing::Point(377, 24);
 			this->button6->Name = L"button6";
-			this->button6->Size = System::Drawing::Size(176, 727);
+			this->button6->Size = System::Drawing::Size(66, 305);
 			this->button6->TabIndex = 5;
 			this->button6->Text = L"A";
 			this->button6->TextAlign = System::Drawing::ContentAlignment::BottomCenter;
@@ -188,10 +187,9 @@ namespace Piano {
 			// 
 			this->button7->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button7->Location = System::Drawing::Point(1197, 57);
-			this->button7->Margin = System::Windows::Forms::Padding(8, 7, 8, 7);
+			this->button7->Location = System::Drawing::Point(449, 24);
 			this->button7->Name = L"button7";
-			this->button7->Size = System::Drawing::Size(176, 727);
+			this->button7->Size = System::Drawing::Size(66, 305);
 			this->button7->TabIndex = 6;
 			this->button7->Text = L"B";
 			this->button7->TextAlign = System::Drawing::ContentAlignment::BottomCenter;
@@ -202,10 +200,9 @@ namespace Piano {
 			// 
 			this->button8->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button8->Location = System::Drawing::Point(1389, 57);
-			this->button8->Margin = System::Windows::Forms::Padding(8, 7, 8, 7);
+			this->button8->Location = System::Drawing::Point(521, 24);
 			this->button8->Name = L"button8";
-			this->button8->Size = System::Drawing::Size(176, 727);
+			this->button8->Size = System::Drawing::Size(66, 305);
 			this->button8->TabIndex = 7;
 			this->button8->Text = L"C1";
 			this->button8->TextAlign = System::Drawing::ContentAlignment::BottomCenter;
@@ -216,10 +213,9 @@ namespace Piano {
 			// 
 			this->button9->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button9->Location = System::Drawing::Point(1581, 57);
-			this->button9->Margin = System::Windows::Forms::Padding(8, 7, 8, 7);
+			this->button9->Location = System::Drawing::Point(593, 24);
 			this->button9->Name = L"button9";
-			this->button9->Size = System::Drawing::Size(176, 727);
+			this->button9->Size = System::Drawing::Size(66, 305);
 			this->button9->TabIndex = 8;
 			this->button9->Text = L"D1";
 			this->button9->TextAlign = System::Drawing::ContentAlignment::BottomCenter;
@@ -230,10 +226,9 @@ namespace Piano {
 			// 
 			this->button10->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button10->Location = System::Drawing::Point(1773, 57);
-			this->button10->Margin = System::Windows::Forms::Padding(8, 7, 8, 7);
+			this->button10->Location = System::Drawing::Point(665, 24);
 			this->button10->Name = L"button10";
-			this->button10->Size = System::Drawing::Size(176, 727);
+			this->button10->Size = System::Drawing::Size(66, 305);
 			this->button10->TabIndex = 9;
 			this->button10->Text = L"E1";
 			this->button10->TextAlign = System::Drawing::ContentAlignment::BottomCenter;
@@ -246,10 +241,9 @@ namespace Piano {
 			this->button11->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->button11->ForeColor = System::Drawing::Color::White;
-			this->button11->Location = System::Drawing::Point(128, 55);
-			this->button11->Margin = System::Windows::Forms::Padding(8, 7, 8, 7);
+			this->button11->Location = System::Drawing::Point(48, 23);
 			this->button11->Name = L"button11";
-			this->button11->Size = System::Drawing::Size(176, 389);
+			this->button11->Size = System::Drawing::Size(66, 163);
 			this->button11->TabIndex = 10;
 			this->button11->Text = L"C#";
 			this->button11->TextAlign = System::Drawing::ContentAlignment::BottomCenter;
@@ -262,10 +256,9 @@ namespace Piano {
 			this->button12->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->button12->ForeColor = System::Drawing::Color::White;
-			this->button12->Location = System::Drawing::Point(320, 55);
-			this->button12->Margin = System::Windows::Forms::Padding(8, 7, 8, 7);
+			this->button12->Location = System::Drawing::Point(120, 23);
 			this->button12->Name = L"button12";
-			this->button12->Size = System::Drawing::Size(176, 389);
+			this->button12->Size = System::Drawing::Size(66, 163);
 			this->button12->TabIndex = 11;
 			this->button12->Text = L"D#";
 			this->button12->TextAlign = System::Drawing::ContentAlignment::BottomCenter;
@@ -278,10 +271,9 @@ namespace Piano {
 			this->button13->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->button13->ForeColor = System::Drawing::Color::White;
-			this->button13->Location = System::Drawing::Point(723, 57);
-			this->button13->Margin = System::Windows::Forms::Padding(8, 7, 8, 7);
+			this->button13->Location = System::Drawing::Point(271, 24);
 			this->button13->Name = L"button13";
-			this->button13->Size = System::Drawing::Size(176, 389);
+			this->button13->Size = System::Drawing::Size(66, 163);
 			this->button13->TabIndex = 12;
 			this->button13->Text = L"F#";
 			this->button13->TextAlign = System::Drawing::ContentAlignment::BottomCenter;
@@ -294,10 +286,9 @@ namespace Piano {
 			this->button14->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->button14->ForeColor = System::Drawing::Color::White;
-			this->button14->Location = System::Drawing::Point(915, 57);
-			this->button14->Margin = System::Windows::Forms::Padding(8, 7, 8, 7);
+			this->button14->Location = System::Drawing::Point(343, 24);
 			this->button14->Name = L"button14";
-			this->button14->Size = System::Drawing::Size(176, 389);
+			this->button14->Size = System::Drawing::Size(66, 163);
 			this->button14->TabIndex = 13;
 			this->button14->Text = L"G#";
 			this->button14->TextAlign = System::Drawing::ContentAlignment::BottomCenter;
@@ -310,10 +301,9 @@ namespace Piano {
 			this->button15->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->button15->ForeColor = System::Drawing::Color::White;
-			this->button15->Location = System::Drawing::Point(1107, 57);
-			this->button15->Margin = System::Windows::Forms::Padding(8, 7, 8, 7);
+			this->button15->Location = System::Drawing::Point(415, 24);
 			this->button15->Name = L"button15";
-			this->button15->Size = System::Drawing::Size(176, 389);
+			this->button15->Size = System::Drawing::Size(66, 163);
 			this->button15->TabIndex = 14;
 			this->button15->Text = L"Bb";
 			this->button15->TextAlign = System::Drawing::ContentAlignment::BottomCenter;
@@ -326,10 +316,9 @@ namespace Piano {
 			this->button16->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->button16->ForeColor = System::Drawing::Color::White;
-			this->button16->Location = System::Drawing::Point(1480, 57);
-			this->button16->Margin = System::Windows::Forms::Padding(8, 7, 8, 7);
+			this->button16->Location = System::Drawing::Point(555, 24);
 			this->button16->Name = L"button16";
-			this->button16->Size = System::Drawing::Size(176, 389);
+			this->button16->Size = System::Drawing::Size(66, 163);
 			this->button16->TabIndex = 15;
 			this->button16->Text = L"C#1";
 			this->button16->TextAlign = System::Drawing::ContentAlignment::BottomCenter;
@@ -342,10 +331,9 @@ namespace Piano {
 			this->button17->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->button17->ForeColor = System::Drawing::Color::White;
-			this->button17->Location = System::Drawing::Point(1672, 57);
-			this->button17->Margin = System::Windows::Forms::Padding(8, 7, 8, 7);
+			this->button17->Location = System::Drawing::Point(627, 24);
 			this->button17->Name = L"button17";
-			this->button17->Size = System::Drawing::Size(176, 389);
+			this->button17->Size = System::Drawing::Size(66, 163);
 			this->button17->TabIndex = 16;
 			this->button17->Text = L"D#1";
 			this->button17->TextAlign = System::Drawing::ContentAlignment::BottomCenter;
@@ -354,9 +342,9 @@ namespace Piano {
 			// 
 			// Piano
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(16, 31);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(2011, 847);
+			this->ClientSize = System::Drawing::Size(747, 354);
 			this->Controls->Add(this->button17);
 			this->Controls->Add(this->button16);
 			this->Controls->Add(this->button15);
@@ -374,7 +362,7 @@ namespace Piano {
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
-			this->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->Margin = System::Windows::Forms::Padding(1, 1, 1, 1);
 			this->Name = L"Piano";
 			this->Text = L"Piano project";
 			this->Load += gcnew System::EventHandler(this, &Piano::Piano_Load);
@@ -383,7 +371,7 @@ namespace Piano {
 		}
 #pragma endregion
 private: System::Void button15_Click(System::Object^  sender, System::EventArgs^  e) {
-	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("Bb.wav");
+	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("sounds\\Bb.wav");
 	player->Load();
 	player->PlaySync();
 }
@@ -391,82 +379,82 @@ private: System::Void Piano_Load(System::Object^  sender, System::EventArgs^  e)
 
 }
 private: System::Void button11_Click(System::Object^  sender, System::EventArgs^  e) {
-	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("C_s.wav");
+	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("sounds\\C_s.wav");
 	player->Load();
 	player->PlaySync();
 }
 private: System::Void button12_Click(System::Object^  sender, System::EventArgs^  e) {
-	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("D_s.wav");
+	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("sounds\\D_s.wav");
 	player->Load();
 	player->PlaySync();
 }
 private: System::Void button13_Click(System::Object^  sender, System::EventArgs^  e) {
-	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("F_s.wav");
+	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("sounds\\F_s.wav");
 	player->Load();
 	player->PlaySync();
 }
 private: System::Void button14_Click(System::Object^  sender, System::EventArgs^  e) {
-	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("G_s.wav");
+	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("sounds\\G_s.wav");
 	player->Load();
 	player->PlaySync();
 }
 private: System::Void button16_Click(System::Object^  sender, System::EventArgs^  e) {
-	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("C_s1.wav");
+	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("sounds\\C_s1.wav");
 	player->Load();
 	player->PlaySync();
 }
 private: System::Void button17_Click(System::Object^  sender, System::EventArgs^  e) {
-	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("D_s1.wav");
+	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("sounds\\D_s1.wav");
 	player->Load();
 	player->PlaySync();
 }
 private: System::Void button4_Click(System::Object^  sender, System::EventArgs^  e) {
-	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("F.wav");
+	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("sounds\\F.wav");
 	player->Load();
 	player->PlaySync();
 }
 private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
-	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("C.wav");
+	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("sounds\\C.wav");
 	player->Load();
 	player->PlaySync();
 }
 private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
-	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("D.wav");
+	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("sounds\\D.wav");
 	player->Load();
 	player->PlaySync();
 }
 private: System::Void button3_Click(System::Object^  sender, System::EventArgs^  e) {
-	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("E.wav");
+	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("sounds\\E.wav");
 	player->Load();
 	player->PlaySync();
 }
 private: System::Void button5_Click(System::Object^  sender, System::EventArgs^  e) {
-	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("G.wav");
+	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("sounds\\G.wav");
 	player->Load();
 	player->PlaySync();
 }
 private: System::Void button6_Click(System::Object^  sender, System::EventArgs^  e) {
-	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("A.wav");
+	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("sounds\\A.wav");
 	player->Load();
 	player->PlaySync();
 }
 private: System::Void button7_Click(System::Object^  sender, System::EventArgs^  e) {
-	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("B.wav");
+	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("sounds\\B.wav");
 	player->Load();
 	player->PlaySync();
 }
 private: System::Void button8_Click(System::Object^  sender, System::EventArgs^  e) {
-	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("C_s1.wav");
+	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("sounds\\C_s1.wav");
 	player->Load();
 	player->PlaySync();
 }
 private: System::Void button9_Click(System::Object^  sender, System::EventArgs^  e) {
-	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("D_s1.wav");
+	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("sounds\\D_s1.wav");
 	player->Load();
 	player->PlaySync();
 }
 private: System::Void button10_Click(System::Object^  sender, System::EventArgs^  e) {
-	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("E1.wav");
+	System::Media::SoundPlayer^ player = gcnew System::Media::SoundPlayer("sounds\\E1.wav");
 	player->Load();
 	player->PlaySync();
 }
